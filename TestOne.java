e63f8e7d8f84cbf2b7f4f467df61e55f1ac2b1f9
Bala1960 <athreyaOne@gmail.com>
2023-07-26 18:03:41 +0530

    GitIntegrationwithJava

---
+++ TestOne.java
@@ -0,0 +1,11 @@
+package com.verizon1;
+
+public class TestOne {
+
+	public static void main(String[] args) {
+		System.out.println("My first java in eclipse");
+		System.out.println("Java is an object");
+
+	}
+
+}
